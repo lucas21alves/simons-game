661786cc01b249a1aca10899f7aa42cca67970bb
lucas21alves <lucas21alves@hotmail.com>
2024-09-21 01:09:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,237 +1,247 @@
-// Initialize Howler.js
-const audio0 = new Howl({
-    src: ["./audio/click0.mp3"], // Path to your audio file
-    preload: true,              // Preload the audio to prevent delays
-    onload: function() {
-      console.log('audio0 loaded and ready to play');
-    }
-});
-
-const audio1 = new Howl({
-    src: ["./audio/click1.mp3"], // Path to your audio file
-    preload: true,              // Preload the audio to prevent delays
-    onload: function() {
-        console.log('audio1 loaded and ready to play');
-    }
-});
-
-const audio2 = new Howl({
-    src: ["./audio/click2.mp3"], // Path to your audio file
-    preload: true,              // Preload the audio to prevent delays
-    onload: function() {
-        console.log('audio2 loaded and ready to play');
-    }
-});
-
-const audio3 = new Howl({
-    src: ["./audio/click3.mp3"], // Path to your audio file
-    preload: true,              // Preload the audio to prevent delays
-    onload: function() {
-        console.log('audio3 loaded and ready to play');
-    }
-});
-
-
-// Function to play a sound corresponding to the square number
-function triggerSound(squareNum) {
-
-    if (squareNum === 0) {
-        audio0.play();
-    }
-
-    else if (squareNum === 1) {
-        audio1.play();
-    }
-
-    else if (squareNum === 2) {
-        audio2.play();
-    }
-
-    else if (squareNum === 3) {
-        audio3.play();
-    }
-
+*{
+    padding: 0;
+    margin: 0;
+    font-family: "Jersey 20", sans-serif;
 }
 
-// Function to generate a random number between 0 and 3 (inclusive)
-function randomNumberGenerator_0_3() {
-    return ( Math.floor( Math.random() * 4 ) )
+html {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
 }
 
-// Array to store the sequence of random numbers
-var sequenceRandomNumArray = [];
-
-// Function to generate and store a new random number in the sequence
-function storeRandomNumber() {
-    sequenceRandomNumArray.push(randomNumberGenerator_0_3())
-};
-
-// Function to display the sequence of squares for the user to follow
-function showingSequence() {
-    let delay = 600;
-
-    for (let randomNumber of sequenceRandomNumArray) {
-        
-        setTimeout(function() {
-            document.querySelectorAll(".square")[randomNumber].classList.add("squareShowing");
-            
-            setTimeout(function() { 
-                document.querySelectorAll(".square")[randomNumber].classList.remove("squareShowing") 
-            }, 500);
-
-            triggerSound(randomNumber);
-
-        }, delay);
-
-        delay += 600; // Increment delay for each square to show in sequence
-    }
-} 
-
-// Array to store the player's sequence of clicks
-var sequencePlayerNumArray = [];
-
-// Function to compare the player's sequence with the generated sequence
-function analysePlayerSequence() {
-
-
-    for (let x = 0; x < sequencePlayerNumArray.length; x++) {
-        
-        if (sequencePlayerNumArray[x] === sequenceRandomNumArray[x]) {
-            
-        }
-
-        else {
-
-            document.querySelector("h1").innerHTML = "<span class='redWarning'>OOOH GAME OVER!</span> (Press <u>Click</u> or <u>Space</u> to play again)";
-
-            sequenceRandomNumArray = [];
-
-            if (highestScore < currentLevel) {
-
-                highestScore = (currentLevel - 1);
-                document.querySelector(".highestScoreNum").innerHTML = highestScore;
-
-            }
-
-            currentLevel = 0;
-
-            waitPlayer = false;
-            
-        }
-            
-    }
-
-    if (sequencePlayerNumArray.length === sequenceRandomNumArray.length) {
-        
-        document.querySelector("h1").innerHTML = "Nice! Press <u>Click</u> or <u>Space</u> to move to the next sequence.";
-        
-        
-        
-        waitPlayer = false;
-    }
-
+body {
+    background-color: darkslateblue;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+    align-items: center;
 }
 
-// Players current Level 
-var currentLevel = 0;
+header{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    font-size: 5rem;
+    margin-top: 13vh;
+}
 
-// Players Highest Score
-var highestScore = 0;
+.gameName {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+}
 
-// Boolean to manage waiting for player input
-var waitPlayer = false;
+.creator {
+    width: 50vw;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    font-size: 2rem;
+    color: white;
+}
 
+.hr {
+    width: 100%;
+}
 
-// Event listener for click events outside the squares to start or restart the game
-document.addEventListener("click", function(Event) {
-    if (waitPlayer === false & !Event.target.classList.contains("square")) {
+.container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+    align-items: center;
+    padding-bottom: 7vh;
+}
 
-        waitPlayer = true;
+h1{
+    font-optical-sizing: auto;
+    font-size: 54px;
+    font-weight: 400;
+    padding-bottom: 9vh;
+    padding-top: 9vh;
+    color: white;
+}
 
-        document.querySelector("h1").textContent = "Pay attention to the sequence and try to repeat it !";
+.squaresContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 3rem;
+    width: 22.2rem; 
+}
 
-        currentLevel++;
-        document.querySelector(".currentLevelNum").innerHTML = currentLevel;
+.square {
+    border: solid 0.3rem;
+    border-radius: 1.5rem;
+    width: 9rem;
+    height: 9rem;
+}
 
-        sequencePlayerNumArray = [];
+.square span {
+    visibility: hidden;
+}
 
-        storeRandomNumber(); 
-        showingSequence();
+.square:hover {
+    opacity: 0.8;
+    border-color: whitesmoke;
+    cursor:pointer;
+    transition: all 0.1s;
+}
 
-        } 
+.squareClicked {
+    opacity: 1 !important;
+    box-shadow: 2px 1px 6px 0.1px whitesmoke;
+    background-color: white !important;
+}
+
+.squareShowing {
+    opacity: 1;
+    border-color: whitesmoke;
+    box-shadow: 2px 1px 6px 0.1px whitesmoke;
+}
+
+.greenSquare {
+    background-color: green;
+}
+
+.redSquare {
+    background-color: red;
+}
+
+.yellowSquare {
+    background-color: yellow;
+}
+
+.blueSquare {
+    background-color: blue;
+}
+
+.keyboardControl {
+    font-size: 2rem;
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    position: absolute;
+    left: 75vw;
+    top: 42vh;
+    width: 9.5rem;
+    border: solid 0.2rem;
+    border-radius: 1rem;
+    padding: 1rem;
+}
+
+h2 {
+    font-size: 2rem;
+    color: white;
+    padding-bottom: 1rem;
+    text-align: center;
+}
+
+.key_cont {
+    margin: 0.4rem;
+    background-color: red;
+    border: solid 0.1rem;
+    border-radius: 2rem;
+    width: 3.2rem;
+    display: flex;
+    justify-content: center;
+}
+
+#i {
+    background-color: green;
+}
+
+#o {
+    background-color: red;
+}
+
+#k {
+    background-color: yellow;
+}
+
+#l {
+    background-color: blue;
+}
+
+.levelAndHighScore {
+    display: flex;
+    font-size: 3.0rem;
+    margin-top: 7vh;
+}
+
+.currentLevelDiv {
+    padding-right: 7rem;
+}
+
+.redWarning {
+    color: red;
+}
+
+@media (max-width: 850px) {
+
+    header{
+        font-size: 5rem;
+        margin-top: 0vh;
+    }
+
+    body {
+        transform: scale(0.78);
     }
     
-);
-
-// Event listener for "Space bar" key press to start or restart the game
-document.addEventListener("keydown", function(Event) {
-    if (Event.code === "Space" & waitPlayer === false) {
-
-        waitPlayer = true;
-
-        document.querySelector("h1").textContent = "Pay attention to the sequence and try to repeat it !";
-
-        currentLevel++;
-        document.querySelector(".currentLevelNum").innerHTML = currentLevel;
-
-        sequencePlayerNumArray = [];
-
-        storeRandomNumber(); 
-        showingSequence();
-
-        } 
+    .creator {
+        width: 100vw;
+        font-size: 2rem;
     }
     
-);
-
-// Event listeners for square clicks to register player input and validate the sequence
-for (var i = 0; i <= 3; i++) {
-
-    document.querySelectorAll(".square")[i].addEventListener("click", function() {
-
-        if (sequencePlayerNumArray.length < sequenceRandomNumArray.length) {
-
-            this.classList.add("squareClicked");
+    h1 {
+        transform: scale(0.88);
+        margin-top: 3vh;
+        margin-bottom: 00vh;
+        padding-top: 0vh;
+        padding-bottom: 0vh;
+        width: 100%;
+        height: 25vh;
+        font-size: 45px;
         
-            setTimeout(() => {
-                this.classList.remove("squareClicked")
-            }, 100);
+    }
 
-            triggerSound(parseInt(this.textContent, 10));
+    .squaresContainer {
+        margin-top: 0vh;
+        gap: 3rem;
+        width: 22.2rem; 
+    }
 
-            sequencePlayerNumArray.push(parseInt(this.textContent, 10));
-            
-            analysePlayerSequence();
+    .keyboardControl {
+        display: none;
+    }
 
-        }
+    .levelAndHighScore {
+        transform: scale(0.80);
+        bottom: 80vh;
+        font-size: 3.0rem;
+    }
 
-    })
-};
+    .currentLevelDiv {
+        width: 9.8rem; 
+        padding-right: 2.00rem;
+    }
 
+    .highestScoreDiv {
+        width: 19.8rem; 
+    }
 
-// Key bindings for "i", "o", "k", "l" to simulate square clicks
-let keysToPlay = ["i", "o", "k", "l"]
-
-for (let y = 0; y < keysToPlay.length; y++) {
-
-    document.addEventListener("keydown", function(Event) {
-
-        if (Event.key === keysToPlay[y] & sequencePlayerNumArray.length < sequenceRandomNumArray.length) {
-
-            document.querySelectorAll(".square")[y].classList.add("squareClicked");
-
-            setTimeout(() => {
-                document.querySelectorAll(".square")[y].classList.remove("squareClicked");
-            }, 100);
-    
-            triggerSound(y);
-    
-            sequencePlayerNumArray.push(y);
-            
-            analysePlayerSequence();
-
-        }
-
-    })
 }
 
